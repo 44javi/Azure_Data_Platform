8489995c40f55e8cf5a304a07a38d4c30d29918d
root <root@Javi.>
2025-07-24 10:03:14 -0700

    added spacing

--- management_groups.tf
+++ management_groups.tf
@@ -1,5 +1,4 @@
 # Root module management_groups
-
 data "azurerm_client_config" "current" {}
 
 # Define the list of countries (Management Groups)
@@ -7,31 +6,37 @@ variable "countries" {
   type    = list(string)
   default = ["USA", "Japan"]
 }
+
 # Create Management Groups
 resource "azurerm_management_group" "cyber_nimbus" {
   display_name                = "Cyber Nimbus"
   parent_management_group_id   = var.root_management_group_id
 }
+
 # Create Management Groups
 resource "azurerm_management_group" "Sandbox" {
   display_name                = "Sandbox"
   parent_management_group_id   = azurerm_management_group.cyber_nimbus.id
 }
+
 # Create Management Groups
 resource "azurerm_management_group" "Platform" {
   display_name                = "Platform"
   parent_management_group_id   = azurerm_management_group.cyber_nimbus.id
 }
+
 # Create Management Groups
 resource "azurerm_management_group" "Management" {
   display_name                = "Management"
   parent_management_group_id   = azurerm_management_group.Platform.id
 }
+
 # Create Management Groups
 resource "azurerm_management_group" "regions" {
   display_name                = "Regions"
   parent_management_group_id   = azurerm_management_group.cyber_nimbus.id
 }
+
 # Create Management Groups for each country
 resource "azurerm_management_group" "countries" {
   for_each                    = toset(var.countries)
